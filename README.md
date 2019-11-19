957500506a029b958bcd73e5c61681fd364000b1
Alex <amontag974@gmail.com>
2019-11-19 11:29:33 -0500

    Add save & load functionality

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 This is a version of hangman that can be played in the terminal.
 You will be able to guess the wrong letter six times before the
-word is revealed, unless you win at which point the game will end.
+secret word is revealed, unless you win at which point the game will end.
 
-The program will read the "5desk.txt" file and will select a random word
+The program will read the "5desk.txt" file and from it will select a random word
 between 5 and 12 characters.
+
+You can save your game during your turn by typing "save". When you run the program,
+you will receive a prompt to load a save by typing "load", or start a new game by
+typing "new". The save file is a txt document that shows the secret word, so no
+cheating!
+
+If you do save your game, you will overwrite the last save so keep this in
+mind before saving.
+
+If you try to load a save that doesn't exist, a new game will start instead.
 
 Done through The Odin Project
--- hangman.rb
+++ hangman.rb
@@ -16,6 +16,42 @@ class Game
         word_list = create_word_list      
         @selected_word = word_list.sample.downcase
         @hidden_word = ["_"] * @selected_word.length
+
+        self.get_game_state
+        self.play_game
+    end
+
+    def get_game_state
+        puts "Would you like to load a save (load) or start a new game (new)?"
+        game_state = gets.chomp
+        while game_state.downcase != "load" && game_state.downcase != "new"
+            puts "Please type 'load' to load an existing game or 'new' to start a new game:"
+            game_state = gets.chomp
+        end
+
+        if game_state.downcase == "load"
+            load_save if File.file?("savefile.txt")
+            puts "No previous save exists. Starting a new game" if !File.file?("savefile.txt")
+        end
+    end
+
+    def save_game
+        savefile = File.open("savefile.txt","w")
+        savefile.puts "#{@@guesses_left}"
+        savefile.puts "#{@selected_word}"
+        savefile.puts "#{@hidden_word.join(" ")}"
+        savefile.puts "#{@@letters_guessed.join(" ")}"
+        savefile.close
+
+    end
+
+    def load_save
+        savefile = File.open("savefile.txt","r")
+        @@guesses_left = savefile.readline.to_i
+        @selected_word = savefile.readline.strip
+        @hidden_word = savefile.readline.split(" ")
+        @@letters_guessed = savefile.readline.split(" ")
+        savefile.close
     end
 
     def display_word
@@ -38,10 +74,16 @@ class Game
         guess = " "
         vaild_characters = "abcdefghijklmnopqrstuvwxyz".split("")
         while !vaild_characters.include?(guess) || letter_already_guessed?(guess)
-            puts "Please guess a letter that hasn't been guessed: "
+            puts "Please guess a letter that hasn't been guessed "
+            puts "\nLetters guessed: #{self.display_letters_guessed}\n"
             guess = gets.chomp.downcase
+            if guess == "save"
+                save_game
+                break
+            end
         end
-        @@letters_guessed << guess
+
+        @@letters_guessed << guess unless guess == "save"
         guess
     end
 
@@ -58,8 +100,10 @@ class Game
         if selected_word.include?(guess)
             puts "\n'#{guess}' is a correct guess!"
             true
+        elsif guess == "save"
+            puts "\nGame Saved!"
         else
-            puts "\n'#{guess}' is not the word!"
+            puts "\n'#{guess}' is not in the word!"
             @@guesses_left -= 1
             false
         end
@@ -83,7 +127,6 @@ class Game
             decoded_word = amend_hidden_word(guess).join(" ")
             if @hidden_word.include?("_")
                 puts "\nHere is the incomplete word:\n #{self.display_word}"
-                puts "\nLetters guessed: #{self.display_letters_guessed}"
                 puts "------------------------"
             end
         end
@@ -93,4 +136,3 @@ class Game
 end
 
 new_game = Game.new
-new_game.play_game
